47750d676fee93b753b2891a85e7ccda8ea4ef52
Jaewook Lee <wookboy00@naver.com>
2025-07-01 01:40:26 +0900

    fix(lndrest): fix cannot marshaling invoice

--- pkg/lndrest/invoice.go
+++ pkg/lndrest/invoice.go
@@ -13,20 +13,20 @@ type Invoice struct {
 	Memo            string       `json:"memo,omitempty"`
 	RPreimage       []byte       `json:"r_preimage,omitempty"`
 	RHash           []byte       `json:"r_hash,omitempty"`
-	Value           int64        `json:"value,omitempty"`
-	ValueMsat       int64        `json:"value_msat,omitempty"`
-	CreationDate    int64        `json:"creation_date,omitempty"`
-	SettleDate      int64        `json:"settle_date,omitempty"`
+	Value           int64        `json:"value,string,omitempty"`
+	ValueMsat       int64        `json:"value_msat,string,omitempty"`
+	CreationDate    int64        `json:"creation_date,string,omitempty"`
+	SettleDate      int64        `json:"settle_date,string,omitempty"`
 	PaymentRequest  string       `json:"payment_request,omitempty"`
 	DescriptionHash []byte       `json:"description_hash,omitempty"`
-	Expiry          int64        `json:"expiry,omitempty"`
+	Expiry          int64        `json:"expiry,string,omitempty"`
 	FallbackAddr    string       `json:"fallback_addr,omitempty"`
-	CltvExpiry      uint64       `json:"cltv_expiry,omitempty"`
+	CltvExpiry      uint64       `json:"cltv_expiry,string,omitempty"`
 	Private         bool         `json:"private,omitempty"`
-	AddIndex        uint64       `json:"add_index,omitempty"`
-	SettleIndex     uint64       `json:"settle_index,omitempty"`
-	AmtPaidSat      int64        `json:"amt_paid_sat,omitempty"`
-	AmtPaidMsat     int64        `json:"amt_paid_msat,omitempty"`
+	AddIndex        uint64       `json:"add_index,string,omitempty"`
+	SettleIndex     uint64       `json:"settle_index,string,omitempty"`
+	AmtPaidSat      int64        `json:"amt_paid_sat,string,omitempty"`
+	AmtPaidMsat     int64        `json:"amt_paid_msat,string,omitempty"`
 	State           InvoiceState `json:"state,omitempty"`
 	IsKeysend       bool         `json:"is_keysend,omitempty"`
 	PaymentAddr     []byte       `json:"payment_addr,omitempty"`
